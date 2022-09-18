374123c19bb2c75a480af9f5353d0eca2b385cb0
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-09-18 20:01:15 +0300

    setup server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hi");
+import { Server } from "socket.io";
+import express from "express";
+
+const io = new Server();
+const app = express();
+
+app.get("/", (req, res) => res.json({ ok: true }));
+
+io.on("connection", () => console.log("connection"));
+
+app.listen(3010, "127.0.0.1");
