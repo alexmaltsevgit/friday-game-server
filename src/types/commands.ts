be69729ec693853e4c621e4471ce316cfc039988
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-10-02 00:34:29 +0300

    add commands

---
+++ src/types/commands.ts
@@ -0,0 +1,21 @@
+export enum SocketRoomEvent {
+  CreateRoom = "room:create",
+  ConnectToRoom = "room:connect",
+  LeaveRoom = "room:leave",
+
+  ConnectedToRoom = "room:connected",
+  JoinedPlayer = "room:player:joined",
+  LeftPlayer = "room:player:left",
+}
+
+export enum SocketPlayerEvent {
+  OpenPlayer = "player:open",
+
+  OpenedPlayer = "player:opened",
+  ChangedPlayerFictionName = "player:fictionName:changed",
+}
+
+export const AllCommands = {
+  ...SocketRoomEvent,
+  ...SocketPlayerEvent,
+};
