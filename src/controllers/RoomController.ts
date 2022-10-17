f154f55e62cbaf5cff53782fb8e62f2870cb8c6f
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-10-17 23:38:22 +0300

    fulfill RoomController 'createRoom' endpoint

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -1,9 +1,15 @@
 import { Socket } from "socket.io";
 import { SocketRoomEvent } from "../types";
-import { RoomService } from "../service/RoomService";
+import { RoomService } from "../service";
 
 export class RoomController {
   constructor(private socket: Socket) {}
 
-  [SocketRoomEvent.CreateRoom] = () => RoomService.createRoom();
+  [SocketRoomEvent.CreateRoom] = async () => {
+    const id = await RoomService.createRoom();
+    if (!id) return;
+
+    this.socket.join(id);
+    this.socket.emit(SocketRoomEvent.ConnectedToRoom, { id });
+  };
 }
