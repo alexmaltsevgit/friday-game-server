5e988533f2197b201e56c2e56bbfcf0c8cf427ab
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-11-07 03:38:02 +0300

    boom !!!

--- src/socket/Room.ts
+++ src/socket/Room.ts
@@ -1,17 +1,55 @@
 import { Client, Room } from "colyseus";
-import { ArraySchema, Schema, type } from "@colyseus/schema";
+import { MapSchema, Schema, type } from "@colyseus/schema";
 
-export class GameState extends Schema {
-  @type(["string"]) players = new ArraySchema<string>();
+import { RoomStatus } from "../types";
+
+type Player = {
+  realName?: string;
+  fictionName?: string;
+};
+
+class PlayerState extends Schema {
+  @type("string") realName?: string;
+  @type("string") fictionName?: string;
+
+  constructor({ realName, fictionName }: Player) {
+    super();
+
+    this.realName = realName;
+    this.fictionName = fictionName;
+  }
+}
+
+class GameState extends Schema {
+  @type("string") ownerId?: string;
+  @type("string") status: RoomStatus = RoomStatus.Awaiting;
+  @type({ map: PlayerState }) players = new MapSchema<PlayerState>();
 }
 
 export class GameRoom extends Room<GameState> {
   onCreate(options: any) {
+    this.setPrivate(true);
     this.setState(new GameState());
   }
 
-  onJoin(client: Client) {
-    this.state.players.push("qwe");
-    client.send("state", this.state);
+  onJoin(client: Client, options: Player) {
+    const { realName, fictionName } = options;
+
+    this.state.players.set(
+      client.sessionId,
+      new PlayerState({ realName, fictionName })
+    );
+
+    this.state.ownerId ??= client.sessionId;
+  }
+
+  onLeave(client: Client) {
+    this.state.players.delete(client.sessionId);
+    if (this.state.players.size === 0) return this.disconnect();
+
+    if (client.sessionId === this.state.ownerId) {
+      const allPlayers = Array.from(this.state.players.keys());
+      this.state.ownerId = allPlayers[0];
+    }
   }
 }
