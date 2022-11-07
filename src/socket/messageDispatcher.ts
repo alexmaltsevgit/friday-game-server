cb739334c182aacc53f01fa8e75d6fe76254ebc9
Alexander Maltsev <orangealexmaltsev@gmail.com>
2022-11-07 22:29:56 +0300

    add ChangePlayerFictionName

--- src/socket/messageDispatcher.ts
+++ src/socket/messageDispatcher.ts
@@ -9,4 +9,11 @@ export const messageDispatcher: Record<
 > = {
   [MessageType.ChangeGameStage]: (room, client, payload) =>
     (room.state.stage = payload.newStage),
+
+  [MessageType.ChangePlayerFictionName]: (room, client, payload) => {
+    const player = room.state.players.get(payload.playerId);
+    if (!player) return;
+
+    player.fictionName = payload.newFictionName;
+  },
 };
--- src/types/messageType.ts
+++ src/types/messageType.ts
@@ -1,3 +1,4 @@
 export enum MessageType {
   ChangeGameStage = "changeGameStage",
+  ChangePlayerFictionName = "changePlayerFictionName",
 }
